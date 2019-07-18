788d48eb00122f336c601ab07a619059d17d133e
Chris Kopack <chris@partsoven.com>
2019-07-18 10:20:43 -0700

    Updated to include ETH and BTC

--- code.py
+++ code.py
@@ -1,37 +1,33 @@
-"""
-This example will access the coindesk API, grab a number like bitcoin value in
-USD and display it on a screen
-If you can find something that spits out JSON data, we can display it!
-"""
 import time
 import board
 from adafruit_pyportal import PyPortal
 
-# You can display in 'GBP', 'EUR' or 'USD'
-CURRENCY = 'USD'
 # Set up where we'll be fetching data from
-#DATA_SOURCE = "https://api.coindesk.com/v1/bpi/currentprice.json"
-#DATA_SOURCE = "https://api.coinbase.com/v2/prices/BCH-USD/spot.json"
-DATA_SOURCE = "https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=USD"
-#DATA_LOCATION = ['bpi', CURRENCY, 'rate_float']
-DATA_LOCATION = ["USD"]
-
-
-def text_transform(val):
-    return "$%d" % val
-
+DATA_SOURCE = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BCH,ETH,BTC&tsyms=USD"
+BCH_LOCATION = ['BCH', 'USD']
+ETH_LOCATION = ['ETH', 'USD']
+BTC_LOCATION = ['BTC', 'USD']
 
 # the current working directory (where this file is)
 cwd = ("/"+__file__).rsplit('/', 1)[0]
-pyportal = PyPortal(url=DATA_SOURCE, json_path=DATA_LOCATION,
+pyportal = PyPortal(url=DATA_SOURCE,
+                    json_path=(BCH_LOCATION, ETH_LOCATION, BTC_LOCATION),
                     status_neopixel=board.NEOPIXEL,
-                    default_bg=cwd+"/bitcoin_background2.bmp",
+                    default_bg=cwd+"/bitcoin_background3.bmp",
                     text_font=cwd+"/fonts/Arial-Bold-24-Complete.bdf",
-                    text_position=(195, 130),
-                    text_color=0x0,
-                    text_transform=text_transform)
-pyportal.preload_font(b'$012345789')  # preload numbers
-pyportal.preload_font((0x00A3, 0x20AC)) # preload gbp/euro symbol
+                    text_position=((150, 55),  # BCH
+                                   (150, 130),
+                                   (150, 200)), # BTC
+                    text_color=(0x00,  # BCH
+                                0x00,  # ETH
+                                0x00), # BTC
+                    #text_wrap=(35, # characters to wrap for quote
+                               #0), # no wrap for author
+                    #text_maxlen=(180, 30), # max text size for quote & author
+                   )
+
+# speed up projects with lots of text by preloading the font!
+pyportal.preload_font()
 
 while True:
     try:
@@ -39,5 +35,4 @@ while True:
         print("Response is", value)
     except (ValueError, RuntimeError) as e:
         print("Some error occured, retrying! -", e)
-
-    time.sleep(3*60)  # wait 3 minutes
+    time.sleep(60)
